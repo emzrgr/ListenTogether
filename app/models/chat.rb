b2f8cd53a7a66bad49fdc7a8a0c5952e185d0c1b
Nicolas Le Roux <nicolaslerouxr@gmail.com>
2025-06-06 10:49:15 +0200

    commiting to pull origin master

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,5 +2,5 @@ class Chat < ApplicationRecord
   belongs_to :user_id
   belongs_to :mood_id
   belongs_to :music_id
-  has_many :messages, depenent: :destroy
+  has_many :messages, dependent: :destroy
 end
